349f610a566ffea807a6e2f88e8f34f43817b8df
dkswnkk <jjoy9810@gmail.com>
2021-08-15 11:30:23 +0900

    splah화면 추가, 앱 로고 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:damo/screen/Internet_check.dart';
+import 'package:damo/screen/loading.dart';
 import 'package:damo/sign/sign.dart';
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk/all.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main() {
   KakaoContext.clientId = '146054d09a25beea21919c465959bafe';
@@ -11,11 +15,15 @@ class Damo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Sign(),
-      theme: ThemeData(
-        focusColor: Colors.grey,
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        home: AnimatedSplashScreen(
+          duration: 1500,
+          splash: 'assets/images/DAMO_logo-03.png',
+          nextScreen: Sign(),
+          splashTransition: SplashTransition.fadeTransition,
+          pageTransitionType: PageTransitionType.fade,
+          backgroundColor: Colors.white,
+          splashIconSize: 300.0,
+        ));
   }
 }
